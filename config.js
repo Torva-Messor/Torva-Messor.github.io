fc268aa5b9e6ed1b7d4bf51f5e136ddb0b63e709
Torva-Messor <marcel.haffstein@gmail.com>
2026-02-09 20:40:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Zentrale Variablen für die API
+const Pub_Nub_api_Publish = "sub-c-6fdfc9e3-91b8-4f79-b62a-6fd9fc69eff7";
+const Pub_Nub_api_Subscribe = "pub-c-f5b11d3c-ddfe-4dfc-963e-6fae738aeb03";
+const My_Channel_Name = "PräsiW.Woche"; // Ein beliebiger Name
+
