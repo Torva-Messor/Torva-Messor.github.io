25277602f8c369158ee771115442bccc85b6f66f
Torva-Messor <marcel.haffstein@gmail.com>
2026-02-09 21:18:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Zentrale Variablen für die API
 const Pub_Nub_api_Publish = "pub-c-f5b11d3c-ddfe-4dfc-963e-6fae738aeb03";
 const Pub_Nub_api_Subscribe = "sub-c-6fdfc9e3-91b8-4f79-b62a-6fd9fc69eff7";
-const My_Channel_Name = "PräsiW.Woche"; // Ein beliebiger Name
+const My_Channel_Name = "PrasiW.Woche"; // Ein beliebiger Name
 
