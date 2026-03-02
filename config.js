99e0c83e58a5d2ac2599b45ff3a2cdf1d2910095
Torva-Messor <marcel.haffstein@gmail.com>
2026-03-02 21:54:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-
-const Pub_Nub_api_Publish = "pub-c-e42eb327-81b5-4680-943e-18aadacbb4a2";
-const Pub_Nub_api_Subscribe = "sub-c-b1fc94a5-5344-468e-ba71-5d5fdd609f56";
-const My_Channel_Name = "PrasiW.Woche";
-
+const CONFIG = {
+    PUBNUB_PUBLISH_KEY: "pub-c-e42eb327-81b5-4680-943e-18aadacbb4a2",
+    PUBNUB_SUBSCRIBE_KEY: "sub-c-b1fc94a5-5344-468e-ba71-5d5fdd609f56",
+    CHANNEL_NAME: "PrasiW.Woche"
+};
